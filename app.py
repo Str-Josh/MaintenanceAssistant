24c0b7ab2471b9b21467f32128d38d068061e6f7
Str-Josh <joshuacarter904@gmail.com>
2025-04-10 16:17:30 -0500

    test3

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import datetime as dt
 import logging
 import os
 
-from mymodels import db, User, Notification, Asset
+from mymodels import db, User, Message, Asset
 import failure_model as f_model
 from mock_hospital import mock_users, mock_notifications, mock_assets
 
@@ -56,7 +56,7 @@ def create_mock_db():
         db.session.add(_user)
         db.session.commit()
     for _mock_notification in mock_notifications:
-        _notification = Notification(
+        _notification = Message(
             sender = _mock_notification["sender"],
             recipient = _mock_notification["recipient"],
             notification_send_date = _mock_notification["notification_send_date"],
@@ -167,7 +167,7 @@ def home():
                             maintenance_required.append(instance)
 
                 # Backend stuff for notifications bar.
-                notifications = Notification.query.filter_by(recipient=current_user.username).all()
+                notifications = Message.query.filter_by(recipient=current_user.username).all()
 
                 return render_template("pages/Manager/ManagerHome.html", assets=maintenance_required, notifications=notifications)
             elif role == 'staff':
@@ -293,7 +293,7 @@ def send_request():
             pass
 
         for _ in maintenance_managers:
-            notification = Notification(
+            notification = Message(
                 sender = current_user.username,
                 recipient = _.username,
                 notification_send_date = today,
@@ -304,7 +304,7 @@ def send_request():
 
             db.session.add(notification)
             db.session.commit()
-            app.logger.info(f"We have added Notification to db for {_.username}.")
+            app.logger.info(f"We have added Message to db for {_.username}.")
         return render_template("pages/PublicAccess/SendMaintenanceRequest.html", success_message="Notice Submitted!")
     return render_template("pages/PublicAccess/SendMaintenanceRequest.html")
 
@@ -407,13 +407,13 @@ def notification_crud_without_c(id, operation):
 @app.route("/delete-notification")
 @login_required
 def delete_notification(id):
-    notification_to_delete = Notification.query.get(id)
+    notification_to_delete = Message.query.get(id)
     if notification_to_delete:
         db.session.delete(notification_to_delete)
         db.session.commit()
-        return f"Notification has successfully been removed.", 200
+        return f"Message has successfully been removed.", 200
     else:
-        return f"Notification was not found.", 404
+        return f"Message was not found.", 404
     
 @app.route("/elevate")
 @login_required
