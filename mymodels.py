898135ecf67f49f9655c98d40aeb50ba66d92555
Str-Josh <joshuacarter904@gmail.com>
2025-04-10 19:41:09 -0500

    1 'i' but 4 eyes and 6 hours still wouldn't have caught it

--- mymodels.py
+++ mymodels.py
@@ -62,7 +62,7 @@ class Asset(db.Model):
 
 class Messages(db.Model):
     __tablename__ = 'Messages'
-    notifcation_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    notification_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     # subject = db.Column(db.String(255))
     # body = db.Column(db.Text)
     # date_sent = db.Column(db.Date)
